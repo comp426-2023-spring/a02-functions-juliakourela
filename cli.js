947ed1c4b22d0e44ab3a0169d4233ae4bf1d3fa8
Jules Kourelakos <julia.kourelakos@duke.edu>
2023-02-08 19:56:58 -0500

    using minimist for arguments now

--- cli.js
+++ cli.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import moment_timezone from 'moment-timezone';
+import moment from 'moment-timezone';
 import node_fetch from 'node-fetch';
 
-if (process.argv[2] && process.argv[2] === '-h') {
+let arg = minimist(process.argv.slice(2))
+
+if (arg.h) {
     console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE`)
     console.log(`    -h            Show this help message and exit.`)
     console.log(`    -n, -s        Latitude: N positive; S negative.`)
@@ -14,3 +16,18 @@ if (process.argv[2] && process.argv[2] === '-h') {
     console.log(`    -j            Echo pretty JSON from open-meteo API and exit.`)
     process.exit(0)
 }
+
+const timezone = moment.tz.guess()
+var latitude = 0;
+var longitude = 0;
+
+if (arg.s && !(arg.n)) latitude = parseFloat(arg.s) * -1;
+else latitude = parseFloat(arg.n);
+
+if (arg.w && !(arg.e)) longitude = parseFloat(arg.w) * -1;
+else longitude = parseFloat(arg.e);
+
+if (arg.z) timezone = arg.z;
+
+// Make a request
+const response = await fetch('');
